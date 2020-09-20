85df0c1d92c0adff379e03fcc58258b3efd800db
Fabian Pena <camilo.fabis@gmail.com>
2020-09-20 06:07:54 -0400

    redoing lab

--- src/App.js
+++ src/App.js
@@ -1,103 +1,86 @@
 import React, { useState } from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
-import foodsJson from './foods.json';
-function App() {
-  let [foods, setFoods] = useState(foodsJson);
-  let [allFoods, setAllFoods] = useState(foodsJson);
-  let [toggle, setToggle] = useState(false);
-  let [newFoodName, setNewFoodName] = useState('');
-  let [newFoodCalories, setNewFoodCalories] = useState('');
+import foodsJSON from './foods.json';
+import FoodBox from './FoodBox.js'
 
+function App() {
+  const [toggle, setToggle] = useState(false);
+  const [foods, setFoods] = useState(foodsJSON)
+  const [foodName, setFoodName] = useState('')
+  const [foodCalories, setFoodCalories] = useState('')
+  const [foodImages, setfoodImages] = useState('')
+  const [allFoods, setallFoods] = useState(foodsJSON);
+
+
+  const showFoods = () => {
+    return foods.map((eachfood) => {
+      return <FoodBox {...eachfood} />;
+    });
+  };
+
+  const showFields = () => {
+    setToggle(!toggle);
+  };
+
+
+  const showForm = () => {
+    return toggle ? (
+      <form onClick={submitFood}>
+        <input
+          placeholder="Enter food"
+          type="text"
+          name="food"
+        ></input>
+        <input
+          placeholder="Enter calories"
+          type="text"
+          name="calories"
+        ></input>
+        <input
+          placeholder="Enter image"
+          type="url"
+          name="image"
+        ></input>
+        <button>Submit</button>
+      </form>
+    ) : null;
+  };
+
+  const submitFood = (e) => {
+    e.preventDefault();
+    // console.log('clicked');
+    let newFood = [...foods]
+    // console.log(newFood)
+    newFood.unshift({
+      name: foodName,
+      calories: foodCalories,
+      image: foodImages
+    });
+    setFoods(newFood)
+    setallFoods(newFood)
+  }
+
+  const searchFood = (e) => {
+    console.log(e.target.value)
+    const result = [...allFoods].filter((food) => food.name.toLowerCase().includes(e.target.value));
+    setFoods(result)
+  }
 
 
   return (
     <div className="App">
-      <button onClick={handleFoodOrder}>Add New Add</button>
-      {toggle ? (
-        <form onSubmit={newFood}>
-          <input
-            style={{ marginTop: '40px' }}
-            placeholder="Enter Food"
-            onChange={foodName}
-            type="text"
-            name="food"
-          />
-          <input
-            style={{ marginTop: '40px' }}
-            placeholder="Enter Calories"
-            onChange={foodCalories}
-            type="text"
-            name="calories"
-          />
-          <button>Submit</button>
-        </form>
-      ) : null}
-      <input onChange={Search} type="text" name="search" placeholder="Search" style={{width: '100%'}} />
-      <FoodBox />
-    </div>
-  );
-  function FoodBox() {
-    return foods.map((eachFood) => {
-      return (
-        <div className="box">
-          <article className="media">
-            <div className="media-left">
-              <figure className="image is-64x64">
-                <img src={eachFood.image} alt="food pics" />
-              </figure>
-            </div>
-            <div className="media-content">
-              <div className="content">
-                <p>
-                  <strong>{eachFood.name}</strong> <br />
-                  <small>{eachFood.calories} cal</small>
-                </p>
-              </div>
-            </div>
-            <div className="media-right">
-              <div className="field has-addons">
-                <div className="control">
-                  <input className="input" type="number" value="1" />
-                </div>
-                <div className="control">
-                  <button className="button is-info">+</button>
-                </div>
-              </div>
-            </div>
-          </article>
-        </div>
-      );
-    });
-  }
-  function handleFoodOrder() {
-    setToggle(!toggle);
-  }
-  function foodName(e) {
-    setNewFoodName(e.target.value);
-  }
-  function foodCalories(e) {
-    setNewFoodCalories(e.target.value);
-  }
-  function newFood(e) {
-    e.preventDefault();
-    let newFoods = [...foods];
-    let someNewFoods = {
-      name: newFoodName,
-      calories: newFoodCalories,
-      image: 'https://i.imgur.com/DupGBz5.jpg',
-      quantity: 0,
-    };
-    newFoods.unshift(someNewFoods);
-    setFoods(newFoods);
-    setAllFoods(newFoods);
-  }
-
-  function Search(e){
-    console.log(e.target.value)
-    let results = [...allFoods].filter(food => food.name.toLowerCase().includes(e.target.value));
-    setFoods(results)
-    
-  }
+      <button onClick={showFields}>Add New Food</button>
+      <input
+        onChange={searchFood}
+        type="text"
+        name="search"
+        placeholder="Search"
+        style={{ width: '100%' }}
+      />
+      {showForm()}
+      {showFoods()}
+     </div>
+  )
 }
 export default App;
